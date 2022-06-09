6978ef92011b511199276e12784f9add41fe2b95
reiend <loncerasjakerei@gmail.com>
2022-06-09 15:58:42 +0800

    added cors, config cors

--- street-goods-burger-backend/config/initializers/cors.rb
+++ street-goods-burger-backend/config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # can be access by all origin/site
+    origins '*'
+
+    resource '*',
+             headers: :any,
+             expose: ['Authorization'],
+             methods: %i[get post put patch delete options show]
+  end
+end
