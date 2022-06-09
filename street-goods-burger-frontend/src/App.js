a57ed2ef0edf75f8556cd95f20d041d041266898
reiend <loncerasjakerei@gmail.com>
2022-06-09 15:10:35 +0800

    Uploaded starter repo

---
+++ street-goods-burger-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "@components/Header/Header.jsx";
+import "@styles/street-goods-burger/core/index.scss";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ street-goods-burger-frontend/src/res/components/Header/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarLogo from "./NavbarLogo.jsx";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <NavbarLogo />
+    </nav>
+  );
+};
+
+export default Navbar;
