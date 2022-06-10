65622d1eb3022dc5f15b81a231c93e1480e97547
reiend <loncerasjakerei@gmail.com>
2022-06-10 22:39:33 +0800

    Minor fixes on specs on checking lengths

--- street-goods-burger-backend/db/schema.rb
+++ street-goods-burger-backend/db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_10_121618) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_10_134907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorite_foods", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "store_customers", force: :cascade do |t|
     t.string "email", default: "", null: false
--- street-goods-burger-backend/spec/models/store_spec.rb
+++ street-goods-burger-backend/spec/models/store_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe "Store\'s Model", type: :model do
@@ -8,14 +10,14 @@ RSpec.describe "Store\'s Model", type: :model do
     )
   end
 
-  describe 'associations' do
+  describe 'Associations' do
     it '1, should have many store_customers' do
       store_customers = Store.reflect_on_association(:store_customers)
       expect(store_customers.macro).to eq(:has_many)
     end
   end
 
-  describe 'attributes' do
+  describe 'Attributes' do
     describe 'store_name' do
       it '1, must be a string' do
         expect(store.store_name).to be_a String
@@ -23,7 +25,7 @@ RSpec.describe "Store\'s Model", type: :model do
       it '2, must be not nil' do
         expect(store.store_name).to_not be_nil
       end
-      it '3, must be greater than 1 and less than 128' do
+      it '3, must be greater than 0 and less than 65' do
         expect(store.store_name.length).to (be >= 1).and(be <= 64)
       end
     end
@@ -35,7 +37,7 @@ RSpec.describe "Store\'s Model", type: :model do
       it '2, must be not nil' do
         expect(store.address).to_not be_nil
       end
-      it '3, must be greater than 1 and less than 128' do
+      it '3, must be greater than 0 and less than 129' do
         expect(store.address.length).to (be >= 1).and(be <= 128)
       end
     end
