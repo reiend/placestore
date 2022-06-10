b73687e170e10ff6496607a15e602e21c6e3d1c7
reiend <loncerasjakerei@gmail.com>
2022-06-10 20:33:42 +0800

    specs for associations StoreCustomer and Store

--- street-goods-burger-backend/spec/models/store_customer_spec.rb
+++ street-goods-burger-backend/spec/models/store_customer_spec.rb
@@ -12,50 +12,60 @@ RSpec.describe 'StoreCustomer\'s Model', type: :model do
       role: 'store_customer'
     )
   end
-  describe 'first_name' do
-    it 'must be a String' do
-      expect(store_customer.first_name).to be_a String
-    end
-    it 'must be not nil' do
-      expect(store_customer.first_name).to_not be nil
-    end
-    it 'must be greater than 1 but less than 50' do
-      expect(store_customer.first_name.length).to (be >= 1).and(be <= 50)
-    end
-  end
-  describe 'last_name' do
-    it 'must be a String' do
-      expect(store_customer.last_name).to be_a String
-    end
-    it 'must be not nil' do
-      expect(store_customer.last_name).to_not be nil
-    end
-    it 'must be greater than 1 but less than 50' do
-      expect(store_customer.last_name.length).to (be >= 1).and(be <= 50)
+
+  describe 'associations' do
+    it '1, should belongs_to store' do
+      store = StoreCustomer.reflect_on_association(:store)
+      expect(store.macro).to eq(:belongs_to)
     end
   end
 
-  describe 'is_ban' do
-    it 'must be a Boolean' do
-      expect(store_customer.is_ban).to be_in([true, false])
+  describe 'attributes' do
+    describe 'first_name' do
+      it '1, must be a String' do
+        expect(store_customer.first_name).to be_a String
+      end
+      it '2, must be not nil' do
+        expect(store_customer.first_name).to_not be nil
+      end
+      it '3, must be greater than 1 but less than 50' do
+        expect(store_customer.first_name.length).to (be >= 1).and(be <= 50)
+      end
     end
-    it 'must be not nil' do
-      expect(store_customer.is_ban).to_not be nil
+    describe 'last_name' do
+      it '1, must be a String' do
+        expect(store_customer.last_name).to be_a String
+      end
+      it '2, must be not nil' do
+        expect(store_customer.last_name).to_not be nil
+      end
+      it '3, must be greater than 1 but less than 50' do
+        expect(store_customer.last_name.length).to (be >= 1).and(be <= 50)
+      end
     end
-    it 'must be false upon creation' do
-      expect(store_customer.is_ban).to be_falsy
-    end
-  end
 
-  describe 'warning' do
-    it 'must be a Integer' do
-      expect(store_customer.warning).to be_a_kind_of(Integer)
+    describe 'is_ban' do
+      it '1, must be a Boolean' do
+        expect(store_customer.is_ban).to be_in([true, false])
+      end
+      it '2, must be not nil' do
+        expect(store_customer.is_ban).to_not be nil
+      end
+      it '3, must be false upon creation' do
+        expect(store_customer.is_ban).to be_falsy
+      end
     end
-    it 'must be not nil' do
-      expect(store_customer.warning).to_not be nil
-    end
-    it 'must be zero upon creation' do
-      expect(store_customer.warning).to be_zero
+
+    describe 'warning' do
+      it '1, must be a Integer' do
+        expect(store_customer.warning).to be_a_kind_of(Integer)
+      end
+      it '2, must be not nil' do
+        expect(store_customer.warning).to_not be nil
+      end
+      it '3, must be zero upon creation' do
+        expect(store_customer.warning).to be_zero
+      end
     end
   end
 end
--- street-goods-burger-backend/spec/models/store_spec.rb
+++ street-goods-burger-backend/spec/models/store_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Store, type: :model do
+RSpec.describe "Store\'s Model", type: :model do
   let(:store) do
     Store.new(
       store_name: Faker::Company.name,
@@ -8,27 +8,36 @@ RSpec.describe Store, type: :model do
     )
   end
 
-  describe 'store_name' do
-    it 'must be a string' do
-      expect(store.store_name).to be_a String
-    end
-    it 'must be not nil' do
-      expect(store.store_name).to_not be_nil
-    end
-    it 'must be greater than 1 and less than 128' do
-      expect(store.store_name.length).to (be >= 1).and(be <= 64)
+  describe 'associations' do
+    it '1, should have many store_customers' do
+      store_customers = Store.reflect_on_association(:store_customers)
+      expect(store_customers.macro).to eq(:has_many)
     end
   end
 
-  describe 'address' do
-    it 'must be a string' do
-      expect(store.address).to be_a String
+  describe 'attributes' do
+    describe 'store_name' do
+      it '1, must be a string' do
+        expect(store.store_name).to be_a String
+      end
+      it '2, must be not nil' do
+        expect(store.store_name).to_not be_nil
+      end
+      it '3, must be greater than 1 and less than 128' do
+        expect(store.store_name.length).to (be >= 1).and(be <= 64)
+      end
     end
-    it 'must be not nil' do
-      expect(store.address).to_not be_nil
-    end
-    it 'must be greater than 1 and less than 128' do
-      expect(store.address.length).to (be >= 1).and(be <= 128)
+
+    describe 'address' do
+      it '1, must be a string' do
+        expect(store.address).to be_a String
+      end
+      it '2, must be not nil' do
+        expect(store.address).to_not be_nil
+      end
+      it '3, must be greater than 1 and less than 128' do
+        expect(store.address.length).to (be >= 1).and(be <= 128)
+      end
     end
   end
 end
