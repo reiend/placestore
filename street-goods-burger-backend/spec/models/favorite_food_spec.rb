8b32b8028c397d70f2704563395c7210863a29e7
reiend <loncerasjakerei@gmail.com>
2022-06-10 22:38:05 +0800

    specs for favorite_food model

---
+++ street-goods-burger-backend/spec/models/favorite_food_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FavoriteFood, type: :model do
+  let(:favorite_food) do
+    FavoriteFood.new(
+      name: Faker::Food.dish,
+      price: Faker::Commerce.price,
+      category: 'food',
+      description: Faker::Food.description,
+      is_available: Faker::Boolean.boolean(true_ratio: 0.5),
+      like: Faker::Number.between(from: 1, to: 100)
+    )
+  end
+
+  describe 'Attributes' do
+    describe 'name' do
+      it '1, must be a string' do
+        expect(favorite_food.name).to be_a String
+      end
+      it '2, must be not nil' do
+        expect(favorite_food.name).to_not be_nil
+      end
+      it '3, must be greater than 0 but less than 65' do
+        expect(favorite_food.name).to (be >= 1).and(be <= 64)
+      end
+    end
+
+    describe 'price' do
+      it '1, must be a some type of Decimal' do
+        expect(favorite_food.price).to be_a BigDecimal
+      end
+      it '2, must be not nil' do
+        expect(favorite_food.price).to_not be_nil
+      end
+      it '3, must be greater than 0' do
+        expect(favorite_food.name).to be >= 1
+      end
+    end
+
+    describe 'category' do
+      it '1, must be a string' do
+        expect(favorite_food.category).to be_a String
+      end
+      it '2, must be not nil' do
+        expect(favorite_food.category).to_not be_nil
+      end
+      it '3, must be greater than 0 but less than 65' do
+        expect(favorite_food.category).to (be >= 1).and(be <= 64)
+      end
+    end
+
+    describe 'description' do
+      it '1, must be a string' do
+        expect(favorite_food.description).to be_a String
+      end
+      it '2, must be not nil' do
+        expect(favorite_food.description).to_not be_nil
+      end
+      it '3, must be greater than 0 but less than 257' do
+        expect(favorite_food.description).to (be >= 1).and(be <= 256)
+      end
+    end
+
+    describe 'is_available' do
+      it '1, must be a boolean' do
+        expect(favorite_food.is_available).to be_in([true, false])
+      end
+      it '2, must be not nil' do
+        expect(favorite_food.is_available).to_not be_nil
+      end
+      it '3, must be greater than 0 but less than 257' do
+        expect(favorite_food.description).to (be >= 1).and(be <= 256)
+      end
+    end
+
+    describe 'like' do
+      it '1, must be a Integer' do
+        expect(favorite_food.like).to be_a_kind_of(Integer)
+      end
+      it '2, must be not nil' do
+        expect(favorite_food.is_available).to_not be_nil
+      end
+      it '3, must be greater than or equal 0' do
+        expect(favorite_food.description).to be >= 1
+      end
+    end
+  end
+end
