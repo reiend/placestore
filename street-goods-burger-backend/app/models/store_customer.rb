6100e49f8554ac1fb3b95591447bf5d60f97849e
reiend <loncerasjakerei@gmail.com>
2022-06-13 18:07:40 +0800

    validations for StoreCustomer

--- street-goods-burger-backend/app/models/store_customer.rb
+++ street-goods-burger-backend/app/models/store_customer.rb
@@ -1,6 +1,18 @@
 class StoreCustomer < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email,
+            :first_name,
+            :last_name,
+            presence: true,
+            length: { minimum: 1, maximum: 64 }
+
+  # check email format
+  validates :email,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, # check email format email format
+            uniqueness: true
+
+  validates :password,
+            length: { minimum: 6 }
+
   belongs_to :store
   has_many :favorite_foods
   has_many :personal_discounts
