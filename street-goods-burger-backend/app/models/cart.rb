d910c2a065a4907b32316c51e85ce06bb69b46d4
reiend <loncerasjakerei@gmail.com>
2022-06-13 21:55:13 +0800

    validation for Cart

--- street-goods-burger-backend/app/models/cart.rb
+++ street-goods-burger-backend/app/models/cart.rb
@@ -1,4 +1,11 @@
 class Cart < ApplicationRecord
   belongs_to :store_transaction
   has_many :food_orders
+
+  validates :quantity,
+            presence: true,
+            numericality: { only_integer: true, greater_than: -1 }
+  validates :total_price,
+            presence: true,
+            numericality: { greater_than: 0.00 }
 end
