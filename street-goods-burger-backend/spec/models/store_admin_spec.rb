cd67ff9a22341bb152387a0321deedded0034896
reiend <loncerasjakerei@gmail.com>
2022-06-13 13:44:16 +0800

    added note that has no specs, no additional column StoreAdmin table

--- street-goods-burger-backend/spec/models/store_admin_spec.rb
+++ street-goods-burger-backend/spec/models/store_admin_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe StoreAdmin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # no test here no additional collumn on the StoreAdmin table
 end
