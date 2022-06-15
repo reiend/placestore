33c429052168d995e89cacd4fdddd312764a3d7d
reiend <loncerasjakerei@gmail.com>
2022-06-15 14:11:50 +0800

    validation for FoodModel

--- street-goods-burger-backend/app/models/food.rb
+++ street-goods-burger-backend/app/models/food.rb
@@ -1,4 +1,15 @@
 class Food < ApplicationRecord
   belongs_to :store
   has_many :reviews
+
+  validates :name, length: { minimum: 1, maximum: 64 }, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than: 0.00 }
+  validates :category, presence: true, length: { minimum: 1, maximum: 64 }, uniqueness: true
+  validates :description, length: { minimum: 1, maximum: 128 }
+  validates :like, numericality: { only_integer: true, greater_than: -1 }
+  validates :is_available, inclusion: { in: [true, false] }
+  validates :discount, presence: true, numericality: { greater_than: 0.00 }
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
+
 end
