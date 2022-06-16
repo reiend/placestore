c05add7039e9d1eb9e871222353e0ebb771fbd91
reiend <loncerasjakerei@gmail.com>
2022-06-16 18:21:57 +0800

    feat for store customer creating a review

--- street-goods-burger-backend/app/models/review.rb
+++ street-goods-burger-backend/app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :food
   belongs_to :store_customer
 
-  validates :reviewer, length: { minimum: 1, maximum: 64 }, uniqueness: true
   validates :text_body, length: { minimum: 1, maximum: 256 }
 end
