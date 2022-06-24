da830256ca0232f7c1a4c32543dcf6b8cc0e07cf
reiend <loncerasjakerei@gmail.com>
2022-06-24 19:41:18 +0800

    added store customer info current store transaction

--- street-goods-burger-backend/app/controllers/store_customer/info_controller.rb
+++ street-goods-burger-backend/app/controllers/store_customer/info_controller.rb
@@ -3,10 +3,19 @@
 # StoreCustomer::InfoController Template
 class StoreCustomer::InfoController < StoreCustomer::StoreCustomerController
   def index
+    store_transaction = current_store_customer.store_transactions.last
+
+    current_store_transaction = if store_transaction[:status] == 'pending'
+                                  store_transaction
+                                else
+                                  'no current store transaction'
+                                end
+
     render json: {
       status: 200,
       message: 'successfully fetch current store customer information',
       data: {
+        current_store_transaction:,
         store_customer: current_store_customer
       }
     }
