1a482ac0df07b029b3e98a91f78f4c3f0e7f68a7
reiend <loncerasjakerei@gmail.com>
2022-07-01 22:22:59 +0800

    return deleted files

---
+++ street-goods-burger-frontend/src/res/components/Header/Navbar/NavbarLogo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import StreetGoodsBurgerLogo from "@globals/components/StreetGoodsBurgerLogo.jsx";
+
+const NavbarLogo = () => (
+  <StreetGoodsBurgerLogo className="street-goods-burger-logo" />
+);
+
+export default NavbarLogo;
