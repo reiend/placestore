f9267109f91d5bbc4b9c2c4a09ae4f63b4dadf51
reiend <loncerasjakerei@gmail.com>
2022-07-18 04:52:17 +0800

    for moving viewing food on store controller, give spec on store viewing foods

---
+++ street-goods-burger-backend/spec/requests/store/store_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe 'StoreController', type: :request do
+  let(:store) do
+    Store.create(
+      store_name: 'store',
+      address: 'adress'
+    )
+  end
+
+  describe 'Get' do
+    describe 'store/foods' do
+      it '1, if store not found raise an error' do
+        get '/store/food', params: {
+          food: {
+            store_id: -1
+          }
+        }
+        expect(JSON.parse(response.body)['status']).to (be == 400).or(be == 422)
+      end
+
+      it '1, if store found respond success' do
+        get '/store/food', params: {
+          food: {
+            store_id: store.id
+          }
+        }
+        expect(JSON.parse(response.body)['status']).to be == 200
+      end
+      it '1, if store found respond data should not be nil' do
+        get '/store/food', params: {
+          food: {
+            store_id: store.id
+          }
+        }
+        expect(JSON.parse(response.body)['data']).to_not be_nil
+      end
+    end
+  end
+end
