f2b48b08c9d40977be4b20054c8ae450a2e6a26f
reiend <loncerasjakerei@gmail.com>
2022-07-18 02:35:20 +0800

    add new specs for food controller

--- street-goods-burger-backend/spec/requests/store/food_spec.rb
+++ street-goods-burger-backend/spec/requests/store/food_spec.rb
@@ -1,20 +1,37 @@
-RSpec.describe 'Store/FoodController', type: :request do
+RSpec.describe 'FoodController', type: :request do
   describe 'Get' do
-    describe 'store/food' do
-      before(:each) do
-        store = Store.create(
+    describe 'store/foods' do
+      let(:store) do
+        Store.create(
           store_name: 'store',
           address: 'adress'
         )
+      end
+
+      it '1, if store can\'t found raise an error' do
         get '/store/food', params: {
           food: {
             store_id: -1
           }
         }
+        expect(JSON.parse(response.body)['status']).to (be == 400).or(be == 422)
       end
 
-      it '1, invalid params info should raise an error' do
-        expect(JSON.parse(response.body)['status']).to (be == 400).or(be == 422)
+      it '1, if store found respond success' do
+        get '/store/food', params: {
+          food: {
+            store_id: store.id
+          }
+        }
+        expect(JSON.parse(response.body)['status']).to be == 200
+      end
+      it '1, if store found respond data should not be nil' do
+        get '/store/food', params: {
+          food: {
+            store_id: store.id
+          }
+        }
+        expect(JSON.parse(response.body)['data']).to_not be_nil
       end
     end
   end
