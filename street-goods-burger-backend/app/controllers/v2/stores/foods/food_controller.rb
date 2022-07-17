cd30e81a0cb9a44e94660fd41edb16e41d9ccdc8
reiend <loncerasjakerei@gmail.com>
2022-07-18 03:55:58 +0800

    replace add_new_food_controller by adding create on food_controller

--- street-goods-burger-backend/app/controllers/v2/stores/foods/food_controller.rb
+++ street-goods-burger-backend/app/controllers/v2/stores/foods/food_controller.rb
@@ -5,7 +5,10 @@ module V2
     module Foods
       # FoodController Template
       class FoodController < StoreController
-        def store_foods
+        before_action :authenticate_store_admin!, except: [:index]
+
+        # for viewing foods on store
+        def index
           # get store foods according to store_id
           store_foods = Store.find(food_params[:store_id]).foods
 
@@ -24,12 +27,41 @@ module V2
           }
         end
 
+        # for creating new food for the store
+        # this includes uploading images after creating on cloudinary
+        def create
+          food_info = add_new_food_params
+
+          add_new_food_info = current_store_admin
+                              .add_new_food(food_info:)
+
+          render json: add_new_food_info
+        rescue ActionController::ParameterMissing => e
+          render json: {
+            status: 400,
+            message: 'invalid parameters',
+            errors: e.message
+          }
+        end
+
         private
 
         def food_params
           params
             .require(:food)
             .permit(:store_id)
+        end
+
+        def add_new_food_params
+          params
+            .require(:food_info)
+            .permit(
+              :name,
+              :category,
+              :description,
+              :price,
+              :picture
+            )
         end
       end
     end
