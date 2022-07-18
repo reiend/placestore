487f5ba1392220a420827477014b14274153f57e
reiend <loncerasjakerei@gmail.com>
2022-07-18 17:27:10 +0800

    updated App test and component

--- street-goods-burger-frontend/src/App.tsx
+++ street-goods-burger-frontend/src/App.tsx
@@ -2,8 +2,14 @@ import React, { Fragment } from 'react';
 import '@libs/reiend/scss/index.scss';
 import '@styles/scss/app.scss';
 
+import StoreLanding from '@components/Store/StoreLanding.tsx';
+
 const App = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <StoreLanding />
+    </Fragment>
+  );
 };
 
 export default App;
--- street-goods-burger-frontend/src/__test__/App.test.tsx
+++ street-goods-burger-frontend/src/__test__/App.test.tsx
@@ -3,29 +3,13 @@ import { render } from '@testing-library/react';
 import App from '@src/App.tsx';
 
 describe('App', () => {
-  type AppChildrenTypes = readonly [string, string, string, string, string];
-  // list of children on app component
-  const appChildren: AppChildrenTypes = [
-    'header',
-    'welcome',
-    'menu',
-    'about',
-    'contacts',
-  ];
-
   it('Should renders without crashing', () => {
     render(<App />);
   });
 
-  it(`Should have ${
-    appChildren.length
-  } children only namely, ${appChildren.join(', ')}`, () => {
+  it('Should have a landing page', () => {
     const { getByTestId } = render(<App />);
 
-    appChildren.forEach(child => {
-      const element = getByTestId(child);
-      expect(element).toBeInTheDocument();
-    });
-    expect(appChildren.length).toBe(5);
+    expect(getByTestId('landing')).toBeInTheDocument();
   });
 });
