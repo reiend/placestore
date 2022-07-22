dac4e44956a10c4a7fd1a5883427d0632442765e
reiend <loncerasjakerei@gmail.com>
2022-07-22 23:28:58 +0800

    updated App component test

--- street-goods-burger-frontend/src/res/components/App/App.test.tsx
+++ street-goods-burger-frontend/src/res/components/App/App.test.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
 import App from './index.ts';
 
 describe('App', () => {
-  // Test if the component renders without crashing
-  it('Should renders without crashing', () => {
+  beforeEach(() => {
     render(<App />);
   });
 
+  // Test if the component renders without crashing
+  it('Should renders without crashing', () => {});
+
   // Test if the component has home component
   it('Should have a Home component', () => {
-    const { getByTestId } = render(<App />);
-
-    expect(getByTestId('home')).toBeInTheDocument();
+    const mainElement = screen.getByTestId('home');
+    expect(mainElement).toBeInTheDocument();
   });
 });
