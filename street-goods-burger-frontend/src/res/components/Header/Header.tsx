86b3a33c2b2fcd1dbbf6255c8dc7d310344a47e3
reiend <loncerasjakerei@gmail.com>
2022-07-22 23:32:49 +0800

    updated Header component, added Wrap StoreLogo into Link for going into Home or root route

--- street-goods-burger-frontend/src/res/components/Header/Header.tsx
+++ street-goods-burger-frontend/src/res/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import AuthenticationButton from './AuthenticationButton/index.ts';
+import StoreLogo from '@globals/components/StoreLogo/index.ts';
+import { Link } from 'react-router-dom';
+
 import './Header.scss';
 
 const Header = () => {
   return (
     <header className='header'>
-      <svg role='store-logo'></svg>
+      <Link to='/home' role='link-to-home'>
+        <StoreLogo role='store-logo' className='store-logo' />
+      </Link>
       <div>
         <AuthenticationButton role='signin-button' title='signin' />
         <AuthenticationButton role='signup-button' title='signup' />
