283e587f93d53229df86d264e9e409490024aded
reiend <loncerasjakerei@gmail.com>
2022-07-25 00:08:31 +0800

    added test Drawer content

---
+++ street-goods-burger-frontend/src/res/components/Header/DrawerContent/DrawerContent.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+
+import DrawerContent from './DrawerContent';
+import { DrawerContentProps } from './utils';
+import { NavbarProps } from '../Navbar/utils';
+
+describe('DrawerContent', () => {
+  const props: DrawerContentProps = {
+    className: 'drawer-content',
+    ['data-testid']: 'drawer-content'
+  };
+
+  const navbarProps: NavbarProps = {
+    items: ['home', 'menu', 'about', 'contacts']
+  };
+
+  beforeEach(() => {
+    render(<DrawerContent {...props} />);
+  });
+
+  // Test if the component have a text home, menu, about, contacts
+  it(`Should render the texts ${navbarProps.items}`, () => {
+    navbarProps.items.forEach(item => {
+      const liElement = screen.getByText(item) as HTMLLIElement;
+      expect(liElement).toBeInTheDocument();
+    });
+  });
+
+  // Test if the component have a signin button
+  it(`Should renders a signin button`, () => {
+    const buttonElement = screen.getByRole('button', {
+      name: 'signin'
+    }) as HTMLButtonElement;
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  // Test if the component have a signup button
+  it(`Should renders a signup button`, () => {
+    const buttonElement = screen.getByRole('button', {
+      name: 'signup'
+    }) as HTMLButtonElement;
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
