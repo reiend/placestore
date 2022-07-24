b36872aab85537eca5c62d840691415cd7000eab
reiend <loncerasjakerei@gmail.com>
2022-07-25 00:12:08 +0800

    added DrawerContent on header

--- street-goods-burger-frontend/src/res/components/Header/Header.tsx
+++ street-goods-burger-frontend/src/res/components/Header/Header.tsx
@@ -3,11 +3,22 @@ import { Link } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Drawer from './Drawer';
+import DrawerContent from './DrawerContent';
 import Brand from '../../globals/components/Brand'; // for some reason using alias here remove types checking
 import Button from '../../globals/components/Button'; // for some reason using alias here remove types checking
 
 import StreetGoodsBurgerLogoUrl from '@assets/StreetGoodsBurgerLogo.jpg';
 import './Header.scss';
+
+// whitelist styles
+// dont purge this styles
+// drawer--open
+// drawer
+// drawer-content
+// drawer-content__navbar
+// drawer-content__navbar--open
+// drawer-content__signup
+// drawer-content__signin
 
 const Header = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -43,6 +54,12 @@ const Header = () => {
         className='navbar'
         items={['home', 'menu', 'about', 'contacts']}
       />
+      {open && (
+        <DrawerContent
+          className='drawer-content'
+          data-testid='drawer-content'
+        />
+      )}
     </header>
   );
 };
