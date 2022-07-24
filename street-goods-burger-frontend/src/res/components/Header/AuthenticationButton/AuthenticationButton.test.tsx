f8ce0a158ed20d2d1c718d5da53759413e8b83e9
reiend <loncerasjakerei@gmail.com>
2022-07-24 10:35:32 +0800

    Update AuthenticationButton Test

--- street-goods-burger-frontend/src/res/components/Header/AuthenticationButton/AuthenticationButton.test.tsx
+++ street-goods-burger-frontend/src/res/components/Header/AuthenticationButton/AuthenticationButton.test.tsx
@@ -5,14 +5,24 @@ import { AuthenticationButtonProps } from './utils';
 
 describe('AuthenticationButton', () => {
   const props: AuthenticationButtonProps = {
-    title: 'signup-button'
+    title: 'signup-button',
+    className: 'signup-button'
   };
 
   beforeEach(() => {
     render(<AuthenticationButton {...props} />);
   });
 
-  const { title } = props;
+  const { className, title } = props;
+
+  // Test if the component has the same className pass on props
+  it('Should have the same className pass on props', () => {
+    // removes null and undefined
+    if (className != null) {
+      const buttonElement = screen.getByText(className) as HTMLButtonElement;
+      expect(buttonElement).toBeInTheDocument();
+    }
+  });
 
   // Test if the component has the same title pass on props
   it('Should have the same title pass on props', () => {
