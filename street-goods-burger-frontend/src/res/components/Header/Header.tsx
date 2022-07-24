11e42f3c9d7e6c747baf4cb531c82f95224f1834
reiend <loncerasjakerei@gmail.com>
2022-07-24 19:29:19 +0800

    Replace AuthenticationButton with generic Button component

--- street-goods-burger-frontend/src/res/components/Header/Header.tsx
+++ street-goods-burger-frontend/src/res/components/Header/Header.tsx
@@ -1,30 +1,48 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import AuthenticationButton from './AuthenticationButton';
 import Navbar from './Navbar';
+import Drawer from './Drawer';
 import Brand from '../../globals/components/Brand'; // for some reason using alias here remove types checking
+import Button from '../../globals/components/Button'; // for some reason using alias here remove types checking
 
 import StreetGoodsBurgerLogoUrl from '@assets/StreetGoodsBurgerLogo.jpg';
-
 import './Header.scss';
 
 const Header = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    setOpen(open => !open);
+  };
+
   return (
     <header className='header'>
-      <Link to='/home' data-testid='link-to-home'>
-        <Brand
-          className='store-logo'
-          data-testid='store-logo'
-          src={StreetGoodsBurgerLogoUrl}
-          alt='Brand Logo'
-          title='Brand Logo'
-        />
-      </Link>
-      <div>
-        <AuthenticationButton title='signin' />
-        <AuthenticationButton title='signup' />
+      <div className='header__top'>
+        <Link to='/home' data-testid='link-to-home'>
+          <Brand
+            className='brand'
+            data-testid='brand'
+            src={StreetGoodsBurgerLogoUrl}
+            alt='Brand Logo'
+            title='Street Goods Burger Logo'
+          />
+        </Link>
+        <div className='header__buttons'>
+          <Button className='signin' title='signin' />
+          <Button className='signup' title='signup' />
+          <Drawer
+            className='drawer'
+            open={open}
+            onClick={handleOpen}
+            data-testid='drawer'
+          />
+        </div>
       </div>
-      <Navbar items={['home', 'menu', 'about', 'contacts']} />
+      <Navbar
+        className='navbar'
+        items={['home', 'menu', 'about', 'contacts']}
+      />
     </header>
   );
 };
