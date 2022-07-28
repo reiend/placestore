3ed6890328600bb3d2b36b7568603190c093f01a
reiend <loncerasjakerei@gmail.com>
2022-07-28 10:36:14 +0800

    updated migration for store and store admin

--- street-goods-burger-backend/db/migrate/20220613055645_add_reference_to_store_admin.rb
+++ street-goods-burger-backend/db/migrate/20220613055645_add_reference_to_store_admin.rb
@@ -1,5 +1,5 @@
 class AddReferenceToStoreAdmin < ActiveRecord::Migration[7.0]
   def change
-    add_reference :store_admins, :store, null: false, foreign_key: true
+    # add_reference :store_admins, :store, null: false, foreign_key: true
   end
 end
