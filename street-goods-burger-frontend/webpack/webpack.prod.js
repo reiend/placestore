8b89dc0077368f216b6d9136165a2534d51e6523
reiend <loncerasjakerei@gmail.com>
2022-07-29 09:04:06 +0800

    remove tailwind configs

--- street-goods-burger-frontend/webpack/webpack.prod.js
+++ street-goods-burger-frontend/webpack/webpack.prod.js
@@ -1,6 +1,4 @@
 const path = require('path'); /* node path */
-const tailwindcss = require('tailwindcss');
-
 const MinicssExtractPlugin = require('mini-css-extract-plugin'); /* extracting css from js*/
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
@@ -30,7 +28,7 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: [[tailwindcss, 'postcss-preset-env', 'autoprefixer']]
+                plugins: [['postcss-preset-env', 'autoprefixer']]
               }
             }
           },
