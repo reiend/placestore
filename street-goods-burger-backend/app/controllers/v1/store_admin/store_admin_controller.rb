44f1387284b9d39e14c3d11a85ceadff645c4861
reiend <loncerasjakerei@gmail.com>
2022-07-29 20:38:08 +0800

    remove old api

--- street-goods-burger-backend/app/controllers/v1/store_admin/store_admin_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module V1
-  # StoreAdmin::StoreAdminController Template
-  class StoreAdmin::StoreAdminController < ApplicationController
-    before_action :authenticate_store_admin!
-  end
-end
