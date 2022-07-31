f9fda4378b1ef0253d488d842e8638fe8fd0bfa0
reiend <loncerasjakerei@gmail.com>
2022-07-31 18:44:03 +0800

    updated store test

--- street-goods-burger-backend/spec/factories/store.rb
+++ street-goods-burger-backend/spec/factories/store.rb
@@ -3,5 +3,14 @@ FactoryBot.define do
   factory :store do
     # store_name { Faker::Company.name }
     # address { Faker::Address.street_address }
+
+    name do
+      Faker::Company.name
+    end
+    line1 { 'line1' }
+    line2 { 'line2' }
+    postal_code { '6666' }
+    city { 'city1' }
+    province { 'province1' }
   end
 end
