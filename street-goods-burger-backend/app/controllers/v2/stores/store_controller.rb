7ed7cc664e8fe59f1c812ae16d1d30451762a05d
reiend <loncerasjakerei@gmail.com>
2022-08-01 13:26:31 +0800

    route for viewing specific store and list of store

--- street-goods-burger-backend/app/controllers/v2/stores/store_controller.rb
+++ street-goods-burger-backend/app/controllers/v2/stores/store_controller.rb
@@ -1,11 +1,10 @@
 module V2
   module Stores
-    # FoodController Template
+    # StoreController Template
     class StoreController < ApplicationController
       before_action :authenticate_store_admin!, only: %i[create]
-
       def create
-        store = current_store_admin.stores.create!(store_params)
+        store = current_store_admin.stores.create!(create_params)
 
         render json: {
           status: {
@@ -32,9 +31,67 @@ module V2
         }
       end
 
+      def find
+        store = Store.find(find_params[:id])
+        render json: {
+          status: {
+            code: 200,
+            message: 'successfully find store'
+          },
+          data: {
+            store:,
+            foods: store.foods
+          }
+        }
+      rescue ActiveRecord::RecordInvalid => e
+        render json: {
+          status: {
+            code: 422,
+            message: 'invalid parameters',
+            errors: e.message
+          }
+        }
+      rescue ActionController::ParameterMissing => e
+        render json: {
+          status: {
+            code: 400,
+            message: 'invalid parameters',
+            errors: e.message
+          }
+        }
+      end
+
+      def list
+        stores = Store.all
+
+        render json: {
+          status: {
+            code: 200,
+            message: 'successfully get list of store'
+          },
+          data: stores
+        }
+      rescue ActiveRecord::RecordInvalid => e
+        render json: {
+          status: {
+            code: 422,
+            message: 'invalid parameters',
+            errors: e.message
+          }
+        }
+      rescue ActionController::ParameterMissing => e
+        render json: {
+          status: {
+            code: 400,
+            message: 'invalid parameters',
+            errors: e.message
+          }
+        }
+      end
+
       private
 
-      def store_params
+      def create_params
         params
           .require(:store)
           .permit(
@@ -44,6 +101,14 @@ module V2
             :postal_code,
             :city,
             :province
+          )
+      end
+
+      def find_params
+        params
+          .require(:store)
+          .permit(
+            :id
           )
       end
     end
